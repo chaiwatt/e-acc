7501f3ad2b42524ee488c7c8b9c74cf19b98bcb0
chaiwatt <chaiwat_thaweechan@yahoo.com>
2025-07-07 20:29:46 +0700

    update

--- app/Http/Controllers/MyTestController.php
+++ app/Http/Controllers/MyTestController.php
@@ -413,7 +413,7 @@ class MyTestController extends Controller
             'margin_right'      => 13,
             'margin_top'        => 10,
             'margin_bottom'     => 0,
-            'tempDir'           => sys_get_temp_dir(),
+            // 'tempDir'           => sys_get_temp_dir(),
         ]);
 
         $stylesheet = file_get_contents(public_path('css/pdf-css/cb.css'));
